9dbc7cf4f363e87b980ca91666dec7579160ba43
kokol12 <kokoleismail886@gmail.com>
2023-04-09 22:35:10 +0300

    lsiting values from db

---
+++ sql_practice/6-list_values.sql
@@ -0,0 +1,7 @@
+-- listing values from  table
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+USE hbtn_0c_0;
+CREATE TABLE IF NOT EXISTS first_table(id INT, name VARCHAR(256));
+
+-- the values
+SELECT * FROM first_table;
